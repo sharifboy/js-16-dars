02c80c1ef09edbacb463e53033cc28ab6ce6766b
sharifboy <sharifboy@gmail.com>
2023-03-31 12:29:26 +0500

    added another text

--- app.js
+++ app.js
@@ -1,4 +1,49 @@
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
-}  
+"use strict";
+
+let foo = "Quryoz";
+console.log(foo);
+
+// Data types
+
+// 1 string
+// 2 number
+// 3 boolean
+// 4 undefined
+// 5 symbol
+// 6 bigInt
+// 7 null
+
+// 1 function
+// 2 array
+// 3 object
+
+// Method
+
+// Function declaration
+
+function abs(a) {
+  return a;
+}
+
+// Function expression
+
+const ads = function (c) {
+  return c;
+};
+
+// Arrow function
+
+const hii = (a) => a;
+
+const cars = ["bmw", "mers"];
+const uzbCars = ["cobalt", "spark"];
+const allCars = cars.concat(uzbCars);
+console.log(allCars);
+
+
+const text = document.querySelector(".text")
+
+for (let index = 0; i < text.length; index++) {
+    const element = array[i];
+    console.log(element);
+}
